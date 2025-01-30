72d3e052ed85231ffcaba34ac0b190c54df9f1b4
LeonardoPerozo12 <leonardo.perozzof@gmail.com>
2025-01-30 18:01:40 -0400

    fixes

--- secure-api/src/Routes/auth.js
+++ secure-api/src/Routes/auth.js
@@ -10,14 +10,14 @@ router.post("/register", async (req, res) => {
     const {username, password} = req.body;
 
     const validUser = await User.findOne({username: username});    
-    if(!validUser){
+    if(validUser){
         return res.status(400).json({ error: 'existing user'})
     }
 
     try {
-        const hashedPassword = await bcrypt.hash(req.body.password, 10);
+        const hashedPassword = await bcrypt.hash(password, 10);
         const newUser = new User({
-            username: req.body.username,
+            username: username,
             password: hashedPassword,
         });
 
